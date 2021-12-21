f81fe600ca47b5195d5b46871c74edd911c12cb2
Briampastor <96449359+Briampastor@users.noreply.github.com>
2021-12-21 18:03:59 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,28 @@
+global.owner = ['51975185269','51937148542'] // Put your number here
+global.mods = [] // Want some help?
+global.prems = [] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://briam/pastor',
+  xteam: 'https://api.xteam.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'Brayanpastor'
+}
+
+// Sticker WM
+global.packname = '✨name-bot✨'
+global.author = 'BRAYAN'
+
+global.multiplier = 69 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
